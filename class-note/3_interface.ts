17dd564df0071f25357d685c43d7997013fb9804
seungyeopme <seungyeop.me.dev@gmail.com>
2021-06-23 04:14:05 +0900

    인터페이스 확장(상속)

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -49,6 +49,20 @@ var obj: StringRegexDictionary = {
 }
 obj['cssFile'] = 'a'
 
-Object.keys(obj).forEach(function(value) {
+Object.keys(obj).forEach(function(value) {});
 
-})
+// 인터페이스 확장
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person {
+   language: string;
+}
+
+var captain: Developer = {
+    language: 'ts',
+    age: 100,
+    name: '캡틴'
+}
