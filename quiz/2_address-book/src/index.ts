7b71bc5e913cab179a1e5f96ca2931af2bd75124
seungyeopme <seungyeop.me.dev@gmail.com>
2021-07-02 17:08:39 +0900

    이넘을 이용한 타입 정의

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -10,6 +10,12 @@ interface Contact {
   phones: PhoneNumberDictionary;
 }
 
+enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
 function fetchContacts(): Promise<Contact[]> {
@@ -78,13 +84,16 @@ class AddressBook {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  // home, office, studio
+  findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
+  // findContactByPhone('office');
+  // findContactByPhone(PhoneType.phone);
 
-  addContact(contact) {
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
