594311bae9e8faef7d589177307ff2e159726cab
seungyeopme <seungyeop.me.dev@gmail.com>
2021-07-10 15:47:06 +0900

    유틸리티 타입 사례 - Pick

---
+++ class-note/13_utility-type.ts
@@ -0,0 +1,23 @@
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    stock: number;
+}
+
+// 상품 목록을 받아오기 위한 API 함수
+function fetchProducts(): Promise<Product[]> {
+
+}
+
+// interface ProductDetail {
+//     id: number; 
+//     name: string; 
+//     price: number
+// }
+
+type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
+function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
+
+}
