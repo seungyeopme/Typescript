c82f8a87f60e370698771d4640fde24c0f514588
seungyeopme <seungyeop.me.dev@gmail.com>
2021-07-17 13:51:29 +0900

    API 함수 타입 구체화

--- project/src/app.ts
+++ project/src/app.ts
@@ -55,8 +55,14 @@ fetchCovidSummary().then((res: any) => {
   console.log(res.C)
 })
 
-function fetchCountryInfo(countryCode: any, status: any) {
-  // params: confirmed, recovered, deaths
+enum CovidStatus {
+  Confirmed = 'confirmed',
+  Recovered = 'recovered',
+  Deaths = 'deaths'
+}
+
+function fetchCountryInfo(countryCode: string, status: any) {
+  // status params: confirmed, recovered, deaths
   const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
   return axios.get(url);
 }
